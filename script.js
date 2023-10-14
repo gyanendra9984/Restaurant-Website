39ac7623abe4094ad7e420eadf0439ab33e51b19
gyanendra9984 <mani15102002@gmail.com>
2023-10-14 18:47:42 +0530

    completed

--- script.js
+++ script.js
@@ -76,4 +76,24 @@ var swiper = new Swiper(".product-slider", {
       },
     });
 
+    var swiper = new Swiper(".recipes-slider", {
+      loop:true,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 75000,
+        disableOnInteraction: false,
+      },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1020: {
+            slidesPerView: 3,
+          },
+        },
+      });
+
     
